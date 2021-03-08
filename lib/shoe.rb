78735fc8bc85e94fe901c33ccb0fddd294d22bd7
Tan Vo <tan10tvo@gmail.com>
2021-03-07 21:19:23 -0700

    commit tree c06d3649bd44cf776f59eab9e5ba7e6af064e574

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -12,3 +20,4 @@ class Shoe
   end
 
 end
+
